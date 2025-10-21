73ca115423c336ad2f6282a90bc06a0b62484975
Abhishek-Iyer1 <iyer.abhishek18@gmail.com>
2025-10-21 17:49:20 -0400

    fix(docker): updated carla version for running in server mode and arguments for gpu access

--- carla.sh
+++ carla.sh
@@ -1,9 +1,9 @@
 docker run \
-  --runtime=nvidia \
+  --gpus all \
   --net=host \
   --user=$(id -u):$(id -g) \
   --env=DISPLAY=$DISPLAY \
   --env=NVIDIA_VISIBLE_DEVICES=all \
   --env=NVIDIA_DRIVER_CAPABILITIES=all \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  carlasim/carla:0.9.16 /bin/bash -c "./CarlaUE4.sh -nosound -quality-level=Low"
+  carlasim/carla:0.9.15 /bin/bash -c "./CarlaUE4.sh -nosound -quality-level=Low"
